360753bf474a243de29d39bb28b44fcaf1183ee0
Andy Johnson <andyrjohnson82@gmail.com>
2017-10-12 11:48:32 -0700

    hooked into requestAnimationFrame for app.js to debounce rendering

--- src/app.js
+++ src/app.js
@@ -17,13 +17,21 @@ export function app({ state, actions, view }, target = document.body) {
   const invoke = [];
   const patch = patchFactory(invoke);
   const store = Store(state, actions);
+  let isRendering = false;
+  function render() {
+    const newNodes = view(store);
+    patch(target, target.childNodes[0], newNodes, oldNodes);
+    oldNodes = newNodes;
+    isRendering = false;
+  }
   let oldNodes;
   if (target.childNodes.length > 0) {
     oldNodes = hydrate(target);
   }
   autorun(() => {
-    const newNodes = view(store);
-    patch(target, target.childNodes[0], newNodes, oldNodes);
-    oldNodes = newNodes;
+    if (!isRendering) {
+      isRendering = true;
+      requestAnimationFrame(render);
+    }
   });
 }
---
+++ test/app.test.js
@@ -0,0 +1,3 @@
+import test from "ava";
+import { app } from "../src/app";
+
